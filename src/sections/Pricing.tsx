485d7c1ac135da0453d932a06ff9673d4375b5f9
Shewale41 <shewalegopal4119@gmail.com>
2025-08-05 12:11:22 +0530

    pricing section done

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,4 +1,5 @@
 import CheckIcon from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge"; //w this tailwind merge we can the take the second value of a css when there exists a same property in the className Section
 
 //sample data for pricing component
 const pricingTiers = [
@@ -58,13 +59,16 @@ export const Pricing = () => {
     <>
     <section className="py-24">
       <div className="container" >
+        <div className="section-heading">
         <h2 className="section-title">Pricing</h2>
         <p className="section-description mt-5 ">
           Free forever.Upgrade for better security,unlimited tasks and exclusive
           features.
         </p>
+        </div>
         {/* //the pricing charts here  */}
-        <div>
+        {/* in the large devices the these pricing sections these divs will be in single line at bottom in ascending order */}
+        <div className=" flex flex-col gap-5 items-center mt-10 lg:flex-row lg:items-end lg:justify-center ">
             {pricingTiers.map(({
               title,
               monthlyPrice,
@@ -73,18 +77,26 @@ export const Pricing = () => {
               inverse,
               features
             }) => (
-              <div className="p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] " >
-                <h3 className="text-lg font-bold text-black/50 " >{title}</h3>
+              <div className={twMerge("p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] mx-w-xs w-full  " , inverse===true && ' border-black bg-black text-white ' )} >
+                <div className="flex justify-between" >
+                <h3 className={twMerge("text-lg font-bold text-black/50 ",inverse===true && 'text-white/50')} >{title}</h3>
+                {/* this popular tag will only be for that one pricing section thats actually populr */}
+                {/* //in this span tag we will our rainbow effect ma-ma-magic starting and ending on the same color and in-between 4 colours */}
+                { popular===true && ( <div className=" inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20 ">
+                 <span className=" bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text "> popular </span>
+                </div> ) }
+                
+                </div>
                 <div className="flex items-baseline gap-1 mt-[30px] " >
                   <span className="text-4xl font-bold tracking-tighter leading-none " >${monthlyPrice}</span>
                   <span className="tracking-tight font-bold text-black/50 ">/month</span>
                 </div>
-                <button className="btn btn-primary mt-[30px] w-full ">{buttonText}</button>
-                <ul>
+                <button className={twMerge("btn btn-primary mt-[30px] w-full ",inverse===true && "text-black bg-white " )}>{buttonText}</button>
+                <ul className="flex flex-col gap-5 mt-8 " >
                   {features.map((feature)=>(
-                    <li>
+                    <li className=" text-sm flex items-center gap-3.5 ">
                      <CheckIcon className="h-6 w-6"/>
-                     <span> {feature} </span> 
+                     <span> {feature} </span>  
                     </li>
                   ))}
                 </ul>
